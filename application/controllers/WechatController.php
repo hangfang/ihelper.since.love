213d6a965462153a9ca1333fc9a5efcf25f3ae50
fanghang <fanghang@fujiacaifu.com>
2016-04-06 17:20:31 +0800

    add

--- application/controllers/WechatController.php
+++ application/controllers/WechatController.php
@@ -4,34 +4,43 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class WechatController extends MY_Controller {
     
     /**
-     *  Content	文本消息内容
+     *  Content     文本消息内容
      *  CreateTime	消息创建时间 （整型）
      *  Description 消息描述
-     *  Format	语音格式，如amr，speex等
-     *  FromUserName	发送方帐号（一个OpenID）
-     *  Label	地理位置信息
+     *  Format      语音格式，如amr，speex等
+     *  FromUserName发送方帐号（一个OpenID）
+     *  Label       地理位置信息
      *  Location_X	地理位置维度
      *  Location_Y	地理位置经度
-     *  MediaId	视频消息媒体id，可以调用多媒体文件下载接口拉取数据。
-     *  MsgId	消息id，64位整型
-     *  MsgType	消息类型: 文本、图片、语音、视频、小视频、位置、链接 
-     *  PicUrl	图片链接
-     *  Scale	地图缩放大小
-     *  ThumbMediaId	视频消息缩略图的媒体id，可以调用多媒体文件下载接口拉取数据。
-     *  Title   消息标题
+     *  MediaId     视频消息媒体id，可以调用多媒体文件下载接口拉取数据。
+     *  MsgId       消息id，64位整型
+     *  MsgType     消息类型: 文本、图片、语音、视频、小视频、位置、链接 
+     *  PicUrl      图片链接
+     *  Recognition 语音识别结果，UTF8编码
+     *  Scale       地图缩放大小
+     *  ThumbMediaId视频消息缩略图的媒体id，可以调用多媒体文件下载接口拉取数据。
+     *  Title       消息标题
      *  ToUserName	开发者微信号
      * @var array
      */
-    public $_format = array(
+    public $_receive_format = array(
             'text' => array('ToUserName', 'FromUserName', 'CreateTime', 'MsgType', 'Content', 'MsgId'),
             'image' => array('ToUserName', 'FromUserName', 'CreateTime', 'MsgType', 'PicUrl', 'MediaId', 'MsgId'),
-            'voice' => array('ToUserName', 'FromUserName', 'CreateTime', 'MsgType', 'MediaId', 'Format', 'MsgId'),
+            'voice' => array('ToUserName', 'FromUserName', 'CreateTime', 'MsgType', 'MediaId', 'Format', 'MsgId', 'Recognition'),
             'video' => array('ToUserName', 'FromUserName', 'CreateTime', 'MsgType', 'MediaId', 'ThumbMediaId', 'MsgId'),
             'shortvideo' => array('ToUserName', 'FromUserName', 'CreateTime', 'MsgType', 'MediaId', 'ThumbMediaId', 'MsgId'),
             'location' => array('ToUserName', 'FromUserName', 'CreateTime', 'MsgType', 'Location_X', 'Location_Y', 'Scale', 'Label', 'MsgId'),
             'link' => array('ToUserName', 'FromUserName', 'CreateTime', 'MsgType', 'Title', 'Description', 'Url', 'MsgId'),
         );
     
+    public $_send_format = array(
+            'text' => array('touser', 'msgtype'=>'text', 'text'=>array('content'=>'')),
+            'image' => array('touser', 'msgtype'=>'image', 'image'=>array('media_id'=>'')),
+            'voice' => array('touser', 'msgtype'=>'voice', 'voice'=>array('media_id'=>'')),
+            'video' => array('touser', 'msgtype'=>'video', 'video'=>array('media_id'=>'', 'thumb_media_id'=>'', 'title'=>'', 'description'=>'')),
+            'music' => array('touser', 'msgtype'=>'music', 'music'=>array('title'=>'', 'description'=>'', 'musicurl'=>'', 'hqmusicurl'=>'', 'thumb_media_id'=>'')),
+            'news' => array('touser', 'msgtype'=>'news', 'news'=>array('articles'=>array('title'=>'', 'description'=>'', 'url'=>'', 'picurl'=>''))),
+        );
     public $_msg = array();
 
 	public function index()
@@ -57,15 +66,36 @@ class WechatController extends MY_Controller {
             libxml_disable_entity_loader(true);
             $msgXml = json_decode(json_encode(simplexml_load_string($data, 'SimpleXMLElement', LIBXML_NOCDATA)), true);
             
-            foreach($this->_format[$msgXml['MsgType']] as $_v){
+            foreach($this->_receive_format[$msgXml['MsgType']] as $_v){
                 $this->_msg[$_v] = $msgXml[$_v];
             }
             
             $this->load->model('WechatModel');
             $suc = $this->WechatModel->saveMessage($this->_msg);
             
-            
-            
+            if($msgXml['MsgType'] === 'text'){
+                $contents = explode(' ', $msgXml['Content']);
+                if(count($contnets) === 1){
+                    $expressCompanyName = array_keys($this->config->item('express_list'));
+                    if(in_array($contents[0], $expressCompanyName)){
+                        $msg = $this->_send_format['text'];
+                        $msg['touser'] = $msgXml['FromUserName'];
+                        $msg['text']['content'] = '抱歉，查无名为“'. $content[0] .'”的快递公司...';
+                        $this->WechatModel->sendMessage($msg);
+                    }elseif(in_array($contents[0], $this->config->item('province_city'))){
+                        $msg = $this->_send_format['text'];
+                        $msg['touser'] = $msgXml['FromUserName'];
+                        $msg['text']['content'] = '抱歉，查无名为“'. $content[0] .'”的地区...';
+                        $this->WechatModel->sendMessage($msg);
+                    }else{
+                        $msg = $this->_send_format['text'];
+                        $msg['touser'] = $msgXml['FromUserName'];
+                        $msg['text']['content'] = '您是说“'. $content[0] .'”吗？';
+                        $this->WechatModel->sendMessage($msg);
+                    }
+                    
+                }
+            }
         }else {
         	log_message('error', 'get message from weixin failed');
         	exit;
--- application/models/WechatModel.php
+++ application/models/WechatModel.php
@@ -2,24 +2,46 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class WechatModel extends MY_Model{
+
+    /**
+     * @todo 从微信获取access_token，并存储于数据库
+     * @return string
+     */
     public function getAccessToken(){
         $this->db->select('token');
+        $this->db->where('insert_time > ', time()-2*3600);
         $this->db->order_by('insert_time', 'desc');
         $this->db->limit(1, 0);
         $query = $this->db->get('wechat_token');
         
-        return $query && $query->num_rows()>0 ? $query->row()->token : '';
-    }
-    
-    public function saveAccessToken($token){
-        if(!$this->db->insert('wechat_token', array('token'=>$token))){
-            error_log('insert into access_token error');
-            return false;
-        }
+        $token = $query && $query->num_rows()>0 ? $query->row()->token : '';
         
-        return true;
+        if($token === ''){
+            $data = array();
+            $data['method'] = 'get';
+            $data['url'] = sprintf('%s/token?grant_type=client_credential&appid=%s&secret=%s', WX_CGI_ADDR, WX_APP_ID, WX_APP_SECRET);
+            $rt = $this->http($data);
+            
+            if(!$rt || isset($rt['errorcode'])){
+                error_log('get access_token from wechat error, msg: '. json_encode($rt));
+                return '';
+            }
+
+            if(strlen($rt['access_token']) > 100){
+                if(!$this->db->insert('wechat_token', array('token'=>$rt['access_token']))){
+                    error_log('insert into access_token error, sql: '. $this->db->last_query());
+                }
+            }
+
+            return $rt['access_token'];
+        }
     }
     
+    /**
+     * @todo 存储用户发过来的微信消息
+     * @param array $msg
+     * @return boolean
+     */
     public function saveMessage($msg){
         
         if(!$this->db->insert('wechat_message', $msg)){
@@ -29,4 +51,75 @@ class WechatModel extends MY_Model{
         
         return true;
     }
+    
+    /**
+     * @todo 回复用户信息
+     * @param array $msg
+     * @return boolean
+     */
+    public function sendMessage($msg){
+        $data['data'] = json_encode($msg);
+        $data['url'] = sprintf('%s/message/custom/send?access_token=%s', WX_CGI_ADDR, $this->getAccessToken());
+        $data['method'] = 'post';
+        $rt = $this->http($data);
+        
+        if(!$rt || isset($rt['errorcode'])){
+            error_log('send wechat message, msg: '. json_encode($rt));
+            return false;
+        }
+
+        if(!$this->db->insert('wechat_return_message', $msg)){
+            error_log('save wechat_send_message error, sql:'. $this->db->last_query());
+        }
+        
+        return true;
+    }
+    
+    private function http($args = array()){
+       $ch = curl_init();
+
+       $args = array_merge(array(
+           'data' => array(),
+           'method' => 'get',
+       ) ,$args);
+       if(strtolower($args['method']) === 'post'){
+           curl_setopt($ch, CURLOPT_POST, true);
+           curl_setopt($ch ,CURLOPT_POSTFIELDS, http_build_query($args['data']));
+       }else{
+           $data = array();
+           foreach ($args['data'] as $key => $value) {
+               $data[] = urlencode($key).'='.urlencode($value);
+           }
+           $data = implode('&', $data);
+           $args['url'] .=(strpos($args['url'] ,'?')==false?'?':'&').$data;
+           $args['url'] = trim($args['url'], '&');
+       }
+       
+       curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); // 跳过证书检查 
+       curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2); // 从证书中检查SSL加密算法是否存在 
+       curl_setopt($ch, CURLOPT_URL, $args['url']);
+       curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+       curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+       $result = curl_exec($ch);
+
+       if($result === false){
+           return array(
+               'errcode' => 1,
+               'errmsg' => 'request interface fatal'
+           );
+       }
+
+       curl_close($ch);
+
+       $return = json_decode($result ,true);
+
+       if(!$return){
+           $return = array();
+           $return['errcode'] = 2;
+           $return['errmsg'] = '微信返回数据非json格式';
+           return $return;
+       }
+       
+       return $return;
+   }
 }
