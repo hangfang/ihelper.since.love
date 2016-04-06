532f63c3c8d2abf4fe4d68c236d00d0f37c7ff2c
fanghang <fanghang@fujiacaifu.com>
2016-04-06 12:53:12 +0800

    add

---
+++ application/libraries/My_Router.php
@@ -0,0 +1,20 @@
+<?php
+class MY_Router extends CI_Router {  
+    var $suffix = 'Controller';  
+
+
+    function MY_Router() {  
+        parent::CI_Router();  
+    }  
+    function set_class($class) {  
+        $this->class = $class . $this->suffix;  
+    }  
+    function controller_name() {  
+        if (strstr($this->class, $this->suffix)) {  
+            return str_replace($this->suffix, '', $this->class);  
+        }  
+        else {  
+            return $this->class;  
+        }  
+    }  
+}  
