2efbdcbfb1fc44d7cb471848ed4155a717788117
fanghang <fanghang@fujiacaifu.com>
2016-04-06 14:25:45 +0800

    add

--- application/config/production/constants.php
+++ application/config/production/constants.php
@@ -4,3 +4,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 define('WX_CGI_ADDR', 'https://api.weixin.qq.com/cgi-bin');
 define('WX_APP_ID', 'wxd7739816540194cb');
 define('WX_APP_SECRET', '82c0a8cdfc3daf060ceccd96016d43ed');
+
+define('WX_TOKEN', 'fanghang2015ieg926400');
+define('WX_ENCODING_AES_KEY', 'QC5CPFmTl06C7j3UYK4YuOdYaz4lBZTJjOywAbDnZwX');
--- application/controllers/WechatController.php
+++ application/controllers/WechatController.php
@@ -9,7 +9,39 @@ class WechatController extends MY_Controller {
 	}
     
     public function message(){
-        
-        print_r($this->input->post());
+        $data = $this->input->input_stream();
+
+      	//extract post data
+		if (!empty($data)){
+                /* libxml_disable_entity_loader is to prevent XML eXternal Entity Injection,
+                   the best way is to check the validity of xml by yourself */
+                libxml_disable_entity_loader(true);
+              	$postObj = simplexml_load_string($data, 'SimpleXMLElement', LIBXML_NOCDATA);
+                $fromUsername = $postObj->FromUserName;
+                $toUsername = $postObj->ToUserName;
+                $keyword = trim($postObj->Content);
+                $time = time();
+                $textTpl = "<xml>
+							<ToUserName><![CDATA[%s]]></ToUserName>
+							<FromUserName><![CDATA[%s]]></FromUserName>
+							<CreateTime>%s</CreateTime>
+							<MsgType><![CDATA[%s]]></MsgType>
+							<Content><![CDATA[%s]]></Content>
+							<FuncFlag>0</FuncFlag>
+							</xml>";             
+				if(!empty( $keyword ))
+                {
+              		$msgType = "text";
+                	$contentStr = "Welcome to wechat world!";
+                	$resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+                	echo $resultStr;
+                }else{
+                	echo "Input something...";
+                }
+
+        }else {
+        	echo "";
+        	exit;
+        }
     }
 }
