263104efdbbe38ffa6f531737c4cda24c434d6cc
fanghang <fanghang@fujiacaifu.com>
2016-04-07 21:26:00 +0800

    add

--- application/models/WeatherModel.php
+++ application/models/WeatherModel.php
@@ -7,7 +7,7 @@ class WeatherModel extends MY_Model{
     public function getWeather($cityid){
         $data = array();
         $data['method'] = 'get';
-        $data['header'] = 'apikey: '. BAIDU_WEATHER_API_KEY;
+        $data['header'] = array('apikey: '. BAIDU_WEATHER_API_KEY);
         $data['url'] = sprintf(BAIDU_WEATHER_API_URL, $cityid);
         return $this->http($data);
     }
