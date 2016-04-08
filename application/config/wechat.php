09ab1d0407de52ba374b61509df2e7bdc2f5e8ea
fanghang <fanghang@fujiacaifu.com>
2016-04-08 16:27:11 +0800

    add

--- application/config/wechat.php
+++ application/config/wechat.php
@@ -2,7 +2,7 @@
 
 return array(
     'daigou' => '代购,香港,HK,Hong Kong,hk',
-    'at' => '关注,好友,添加,王琳玲,方航,琰琰,小方',
+    'at' => '你,关注,好友,添加,王琳玲,方航,琰琰,小方',
     'news' => '资讯,news,NEWS',
     'position' => '我,位置',
 );
