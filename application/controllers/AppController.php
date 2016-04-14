8ac18573a7b16701a6a02dc62a820c73ef496556
fanghang <fanghang@fujiacaifu.com>
2016-04-14 12:56:09 +0800

    add news

--- application/controllers/AppController.php
+++ application/controllers/AppController.php
@@ -431,7 +431,7 @@ EOF;
         $data = array();
         $this->input->get('keyword') && $data['word'] = $this->input->get('keyword');
         
-        $data['page'] = $this->input->get('page') ? $this->input->get('page') : rand(1,999);
+        $data['page'] = $this->input->get('page') ? $this->input->get('page') : rand(1,9999);
         
         $data['rand'] = 1;
         $data['num'] = 8;
