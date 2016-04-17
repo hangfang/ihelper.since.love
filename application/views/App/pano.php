85b5d121411e9029f74a123d440ff0dc663eb6f4
hangfang <fangh@me.com>
2016-04-17 23:05:45 +0800

    update

--- application/views/App/pano.php
+++ application/views/App/pano.php
@@ -228,17 +228,19 @@
     
     txmap.init();
     
-    wx.getLocation({
-        type: 'wgs84', // 默认为wgs84的gps坐标，如果要返回直接给openLocation用的火星坐标，可传入'gcj02'
-        success: function (res) {
-            txmap.latLong = {
-                                latitude: res.latitude,// 纬度，浮点数，范围为90 ~ -90
-                                longitude: res.longitude// 经度，浮点数，范围为180 ~ -180。
-                            }
-            txmap.speed = res.speed; // 速度，以米/每秒计
-            txmap.accuracy = res.accuracy; // 位置精度
-            
-            txmap.map.setCenter(new qq.maps.LatLng(res.latitude, res.longitude));
-        }
+    wx.ready(function(res){
+        wx.getLocation({
+            type: 'wgs84', // 默认为wgs84的gps坐标，如果要返回直接给openLocation用的火星坐标，可传入'gcj02'
+            success: function (res) {
+                txmap.latLong = {
+                                    latitude: res.latitude,// 纬度，浮点数，范围为90 ~ -90
+                                    longitude: res.longitude// 经度，浮点数，范围为180 ~ -180。
+                                }
+                txmap.speed = res.speed; // 速度，以米/每秒计
+                txmap.accuracy = res.accuracy; // 位置精度
+
+                txmap.map.setCenter(new qq.maps.LatLng(res.latitude, res.longitude));
+            }
+        });
     });
 </script>
