5221a6f082b51b7c983dd371af03e2dc576ca2c9
fanghang <fanghang@fujiacaifu.com>
2016-04-18 12:36:49 +0800

    add map

--- static/public/js/map/index.js
+++ static/public/js/map/index.js
@@ -350,7 +350,7 @@ $(function(){
                 //设置地图
                 map : txmap.map,
 
-                complete : function(results){
+                complete : function(results){console.log(results);
                     txmap.latLng = results.detail.latLng;
                     txmap.map.setCenter(txmap.latLng);
                     var marker = new qq.maps.Marker({//设置marker标记
