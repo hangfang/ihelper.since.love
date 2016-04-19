afe1342c3e2e0f0046a405b9745209b24e02891e
hangfang <fangh@me.com>
2016-04-19 22:21:42 +0800

    add map service

--- static/public/js/app/girls.js
+++ static/public/js/app/girls.js
@@ -1,11 +1,14 @@
 $(function(){
     $('#container').on('touchstart', '.container-fluid:last', function(e){
-        news.touchstart = event.changedTouches[0].clientY;
+        girls.touchstart = event.changedTouches[0].clientY;
     }).on('touchend', '.container-fluid:last', function(e){
-        news.touchsend = event.changedTouches[0].clientY;
-        var match = location.search.match(/page=(\d+)/);
-        page = match ? match[1]-0+1 : 1;
-        location.href = '/app/girls?page='+page;
-        return false;
+        girls.touchsend = event.changedTouches[0].clientY;
+        
+        if(girls.touchstart-girls.touchend > 100){
+            var match = location.search.match(/page=(\d+)/);
+            page = match ? match[1]-0+1 : 1;
+            location.href = '/app/girls?page='+page;
+            return false;
+        }
     });
 });
--- static/public/js/app/news.js
+++ static/public/js/app/news.js
@@ -3,9 +3,11 @@ $(function(){
         news.touchstart = event.changedTouches[0].clientY;
     }).on('touchend', '.container-fluid:last', function(e){
         news.touchsend = event.changedTouches[0].clientY;
-        var match = location.search.match(/page=(\d+)/);
-        page = match ? match[1]-0+1 : 1;
-        location.href = '/app/news?page='+page;
-        return false;
+        if(girls.touchstart-girls.touchend > 100){
+            var match = location.search.match(/page=(\d+)/);
+            page = match ? match[1]-0+1 : 1;
+            location.href = '/app/news?page='+page;
+            return false;
+        }
     });
 });
