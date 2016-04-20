2f7d8d6f3a6adbfab59f90de87f065dff1ce12a2
fanghang <fanghang@fujiacaifu.com>
2016-04-20 09:47:04 +0800

    fixbug

--- static/public/js/app/news.js
+++ static/public/js/app/news.js
@@ -3,7 +3,7 @@ $(function(){
         news.touchstart = event.changedTouches[0].clientY;
     }).on('touchend', '.container-fluid:last', function(e){
         news.touchend = event.changedTouches[0].clientY;
-        if(girls.touchstart-girls.touchend > 100){
+        if(news.touchstart-news.touchend > 100){
             var match = location.search.match(/page=(\d+)/);
             page = match ? match[1]-0+1 :2;
             location.href = '/app/news?page='+page;
